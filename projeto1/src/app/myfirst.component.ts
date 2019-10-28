4a19d8cc41a6c2d4bb0c1b3c56a4e0a58d901a2b
Marcelo Neves <marcelorarz@outlook.com>
2019-10-28 16:57:43 -0300

    Primeiro commit

---
+++ projeto1/src/app/myfirst.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+
+@Component({
+   selector: 'myfirst',
+  //  template: `
+  //  <h1> My first component </h1>
+  //   `
+  templateUrl: './myfirst.component.html',
+ // styles: [`  
+ // .box1 {
+ //   width: 200px;
+ //   height: 120px;
+ //   background: chocolate;
+ //  box-shadow: 5px 10px #cccccc;
+ // }  
+ // `]
+ styleUrls:['./myfirst.component.css']
+})
+
+export class MyFirstComponent {
+
+}
